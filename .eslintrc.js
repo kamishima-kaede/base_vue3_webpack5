1c9e7ff7a52a3ade8696ba70283a0d931a46840d
shiki ryougi <shikiryougi@shikideMacBook.local>
2023-03-24 14:29:41 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,37 +2,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+  ],
+  overrides: [
+  ],
   parserOptions: {
-    parser: '@babel/eslint-parser',
-    ecmaVersion: 12,
-    sourceType: 'module'
+    ecmaVersion: 'latest',
   },
-  plugins: ['prettier'],
+  plugins: [
+    'vue',
+  ],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 120,
-        tabWidth: 2,
-        useTabs: false,
-        semi: false,
-        singleQuote: true,
-        quoteProps: 'as-needed',
-        jsxSingleQuote: true,
-        trailingComma: 'none',
-        bracketSpacing: true,
-        arrowParens: 'always',
-        htmlWhitespaceSensitivity: 'ignore',
-        vueIndentScriptAndStyle: true,
-        endOfLine: 'auto'
-      },
-      { usePrettierrc: true }
-    ],
-    'no-console': 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/comment-directive': 'off'
-  }
+  },
 };
